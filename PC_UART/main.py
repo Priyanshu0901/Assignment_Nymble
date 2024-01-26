d1ba4d2cb167d29b688678ddce6fec6331365aa6
Priyanshu Roy <52269169+Priyanshu0901@users.noreply.github.com>
2024-01-27 05:01:07 +0530

    serial read working

--- PC_UART/main.py
+++ PC_UART/main.py
@@ -2,14 +2,23 @@ import serial
 
 ser = serial.Serial('COM10', baudrate=2400, timeout=1)
 
+
 try:
     while True:
-        serial_message = ser.readline().decode('utf-8').strip()
-
-        print("Received:", serial_message)
+        # Read a line from the serial port
+        serial_message = ser.readline()
+        
+        try:
+            # Attempt to decode the message as UTF-8
+            decoded_message = serial_message.decode('utf-8').strip()
+            print("Received:", decoded_message)
+        except UnicodeDecodeError as e:
+            # Print the hexadecimal representation of the problematic byte
+            print(f"Error decoding message: {e}. Hex representation: {serial_message.hex()}")
 
 except KeyboardInterrupt:
     print("Script terminated by user.")
 
 finally:
     ser.close()
+
