7df122e0e28ee4e66fc4365a1f2cf7e7f5c7ae1d
Priyanshu Roy <52269169+Priyanshu0901@users.noreply.github.com>
2024-01-27 04:42:45 +0530

    First Commit to branch

--- PC_UART/main.py
+++ PC_UART/main.py
@@ -1,19 +1,15 @@
 import serial
 
-# Replace 'COMx' with the actual serial port you are using (e.g., 'COM1' on Windows, '/dev/ttyUSB0' on Linux)
 ser = serial.Serial('COM10', baudrate=2400, timeout=1)
 
 try:
     while True:
-        # Read a line from the serial port
         serial_message = ser.readline().decode('utf-8').strip()
-        
-        # Print the received message
+
         print("Received:", serial_message)
 
 except KeyboardInterrupt:
     print("Script terminated by user.")
 
 finally:
-    # Close the serial port when done
     ser.close()
